cb43ba82330800f77aed1e98c8e67b59cfdac3c5
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-27 15:27:32 -0800

    Write second test for romanNumerals
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>

--- __tests__/romanNumerals.test.js
+++ __tests__/romanNumerals.test.js
@@ -4,4 +4,8 @@ describe (romanNumerals, () => {
   test("It will only accept input of the letters i, v, x, l, c, d, m", () => {
     expect(romanNumerals("invalid")).toEqual("Invalid Roman numerals detected. Please check your input and try again.");
   });
+
+  test("It will not accept input of double v (vv)", () => {
+    expect(romanNumerals("vv")).toEqual("Invalid input. Numeral V should only occur once per input.");
+  });
 })
