a78419d3146f9cfd3b2c25f9f18276599338ee62
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-27 15:28:01 -0800

    Pass second test for romanNumerals
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>

--- src/js/romanNumerals.js
+++ src/js/romanNumerals.js
@@ -1,5 +1,7 @@
 export const romanNumerals = (input) => {
   if (!input.match(/^[ivxlcdm]+$/i)) {
     return "Invalid Roman numerals detected. Please check your input and try again."
+  } else if (input.includes("vv")) {
+    return "Invalid input. Numeral V should only occur once per input."
   }
 }
