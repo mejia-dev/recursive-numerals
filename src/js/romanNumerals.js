323430ac4f0299bc44d8f9e3b428191d776ddf81
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-27 15:29:25 -0800

    Pass third test for romanNumerals
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>

--- src/js/romanNumerals.js
+++ src/js/romanNumerals.js
@@ -3,5 +3,7 @@ export const romanNumerals = (input) => {
     return "Invalid Roman numerals detected. Please check your input and try again."
   } else if (input.includes("vv")) {
     return "Invalid input. Numeral V should only occur once per input."
+  } else if (input.includes("ll")) {
+    return "Invalid input. Numeral L should only occur once per input."
   }
 }
